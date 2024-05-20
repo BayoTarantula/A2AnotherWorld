80c34437a5906567be5dad2d5a254b8f1c0bc625
BayoTarantula <737644@newcollege.ac.uk>
2024-05-20 16:17:36 +0100

    First Commit
    My first commit of my A2 'Another World' project

---
+++ Assets/Player2DAnimation8DirectionMovementIn3DWorld.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    private bool isFacingRight = false;
+
+    [SerializeField] private Rigidbody2D rb;
+    Animator animator;
+    // Update is called once per frame
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("MovementX", horizontal);
+        animator.SetFloat("MovementY", vertical);
+
+        if (horizontal == 0 && vertical == 0)
+        {
+            animator.SetBool("Moving", false);
+        }
+        else
+        {
+            animator.SetBool("Moving", true);
+        }
+
+        FlipSpriteIn3D();
+    }
+
+    private void FlipSpriteIn3D()
+    {
+        if (isFacingRight && horizontal > 0f || !isFacingRight && horizontal < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
---
+++ Assets/Player3DMOVEMENTFixedPerspective.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+        
+    private float horizontal;
+    private float vertical;
+    private float speed = 10f;
+    
+    [SerializeField] private Rigidbody rb;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed * 2);
+    }
+}
